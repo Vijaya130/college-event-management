39d5b42b5de0989228c1bc1227bcd70750d99e0f
Varsha-Poojary13 <varshakpoojary13@gmail.com>
2026-04-12 14:57:13 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,1 @@
-const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'varsha@132006KV',
-  database: 'college_events'
-});
-
-db.connect((err) => {
-  if (err) {
-    console.log('❌ Database connection failed:', err);
-  } else {
-    console.log('✅ Connected to MySQL database!');
-  }
-});
-
-module.exports = db;
